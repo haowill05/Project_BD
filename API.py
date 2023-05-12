5073712523952a745f0854fbe759deaac887ff24
haowill05 <133372665+haowill05@users.noreply.github.com>
2023-05-12 17:14:23 +0200

    Migrated dbquery and mysqlconnector

--- API.py
+++ API.py
@@ -1,29 +1,11 @@
-from flask import Flask, jsonify, render_template
-from mysql import connector
-from films import getFilms, get_fav_films, get_user_info
-from gameCenter import getGames
+from flask import Flask, jsonify, render_template, redirect, session, request
+from utilities import *
 
 # * Default flask project (don't change)
 app = Flask(__name__)
 app.secret_key = "secret_key"
+app.config['SESSION_TYPE'] = 'filesystem'
 
-# * Database connection details
-connection = connector.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    password="root",
-    database="Project"  # was "testingFlask", change if shit fucks up
-)
-
-# * Creating the cursor
-cursor = connection.cursor()
-
-
-# *################################*#
-
-
-# * Pages the user is gonna visit
 
 @app.route("/", methods=['GET'])
 def homeTest():
@@ -41,12 +23,15 @@ def home():
 
 @app.route("/dashboard", methods=['GET'])
 def dashboard():
-    return render_template("dashboard.html")
+	if not authenticate(session):
+		message = "401 Unauthenticated"
+		return error(message)
+	return render_template("dashboard.html")
 
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
-    return render_template("login.html")
+    return render_template("./login.html")
 
 @app.route("/games", methods=['GET'])
 def viewGames():
@@ -56,29 +41,34 @@ def viewGames():
 # *################################*#
 
 # * API Pages -- User should usually not go on these sites#
+@app.route("/api/login", methods=['POST'])
+def dologin():
+	email = request.form.get('email')
+	userid = dbquery(f"SELECT IdUser FROM User WHERE email = \'{email}\'")
+	
+	if not userid:
+		message = "User does not exist!"
+		return error(message)
+	
+	setSession(session,userid)
+
+	return redirect("/dashboard")
+
 @app.route("/api/games", methods=['GET'])  # Accepting the methods ['GET'], ['POST', 'GET']
 def games():
-    result = getGames(connection, cursor)
-    return jsonify(result)
+	if not authenticate(session):
+		message = "401 Unauthenticated"
+		return error(message)
+	return ""
     # return render_template("front.html") # * Renders the HTML page
-
-
-@app.route("/api/films", methods=['GET'])
-def films():
-    result = getFilms(connection, cursor)
-    return jsonify(result)  # * Returns the result is JSON, easier to work with
 
 
 @app.route("/api/<user>/infos", methods=['GET'])
 def user_infos(user):
-    result = get_user_info(connection, cursor, user)
-    return jsonify(result)
-
-
-@app.route("/api/<user>/favourite_films", methods=['GET'])
-def fav_film(user):
-    result = get_fav_films(connection, cursor, user)
-    return jsonify(result)
+	if not authenticate(session):
+		message = "401 Unauthenticated"
+		return error(message)
+	return ""
 
 
 # *################################*#
@@ -91,3 +81,4 @@ def fav_film(user):
 
 # * Running the app
 app.run(debug=True)
+
